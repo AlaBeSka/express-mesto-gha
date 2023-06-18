688b30a81b88c18fd6e4a7d3c9eb81cd0992119b
AlaBeSka <ehanbegov@yandex.ru>
2023-06-18 15:04:55 +0300

    feat: начало бэка

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/index');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '648ef2829f015f4ce7620578',
+  };
+
+  next();
+});
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Started on ${ PORT }`);
+});
